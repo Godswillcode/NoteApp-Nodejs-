ae233de0ff79c7b32fbfb1d087a3cbfec5938ddd
Onyekachi Godswill <omenukooyekachi690@gmail.com>
2021-04-22 15:43:29 +0100

    added public

--- app.mjs
+++ app.mjs
@@ -18,3 +18,22 @@ export const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("views engine", "hbs");
 hbs.registerPartials(path.join(__dirname, "partials"));
+
+app.use(logger('dev'));
+app.use(bodyParser.json())
+app.use.apply(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', indexRouter);
+
+
+app.use(handle404);
+app.use(basicErrorHanler);
+
+export const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+export const server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
